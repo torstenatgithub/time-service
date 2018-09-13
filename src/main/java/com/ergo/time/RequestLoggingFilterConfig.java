bf195b237e7627ace4bd021ac9219efd1b8a508c
Torsten Kramer <torsten.kramer@itergo.com>
2018-09-13 16:14:54 +0200

    logging added

---
+++ src/main/java/com/ergo/time/RequestLoggingFilterConfig.java
@@ -0,0 +1,19 @@
+package com.ergo.time;
+
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+ 
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+          = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
